f795720496d498d0dabeb35155448ee51310c097
zwx <465606342@qq.com>
2020-07-11 15:21:44 +0800

    提交

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Hello World!");
+    }
+
+    @GetMapping("test")
+    public String test() {
+        System.out.println("------------------------");
+        return "success";
+    }
+}
